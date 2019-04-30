57c00658f9b91f9d9c45be4cb6dd7ce94bd7bace
Riccardo <reymury@gmail.com>
2019-04-30 14:28:16 +0200

    Add some implementation to the methods in PlayerBoard class.

--- src/main/java/it/polimi/se2019/model/player/PlayerBoard.java
+++ src/main/java/it/polimi/se2019/model/player/PlayerBoard.java
@@ -1,66 +1,68 @@
 package it.polimi.se2019.model.player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Player board contains all the logic related to damages, marks and scores
  */
-public abstract class PlayerBoard {
+public class PlayerBoard {
   public PlayerBoard() {
+    marksAssigned = new ArrayList<Player>();
+    damageReceived = new ArrayList<Player>();
+    deathValues = new ArrayList<Integer>();
+    deathValues.add(8);
+    deathValues.add(6);
+    deathValues.add(4);
+    deathValues.add(2);
+    deathValues.add(1);
+    isFrenzy = false;
   }
 
   /**
    * The list of marks the player has been assigned. Marks become damage when the players is attacked by the player
    * who assigned the marks.
    */
-  private List<Player> MarksAssigned;
+  private List<Player> marksAssigned;
 
   /**
    * The ordered list of players who dealt damage to this player. A player appears on the least once for every point
    * of damage dealt
    */
-  private List<Player> DamageReceived;
-
-  /**
-   * The number of times this player has died
-   */
-  private Integer deaths;
+  private List<Player> damageReceived;
 
   /**
    * The amount of point to be assigned for killing this player. The first position contains the number of points to
    * be assigned to the player who dealt the most damage, in second position is the number of points to be given to
    * the player who dealt second to most damage etc.
    */
-  private List<Integer> DeathValue;
+  private List<Integer> deathValues;
 
   /**
    * The side the player's board is on currently. The player board has two sides : one for normal play and one for
    * the final frenetic round.
    */
-  private boolean boardSide;
+  private boolean isFrenzy;
 
   /**
    * @return the list of mark assigned to the current player
    */
   public List<Player> getMarksAssigned() {
+    return marksAssigned;
   }
 
   /**
    * @return the list of damages assigned to the current player
    */
   public List<Player> getDamageReceived() {
-  }
-
-  /**
-   * @return the number of deaths of this player
-   */
-  public Integer getDeaths() {
+    return damageReceived;
   }
 
   /**
    * @return the amount of points to be assigned to those who kill this player.
    */
   public List<Integer> getDeathValue() {
+    return deathValues;
   }
 
   /**
@@ -69,6 +71,7 @@ public abstract class PlayerBoard {
    * @param player The player that did damage
    */
   public void setDamage(Player player) {
+    damageReceived.add(player);
   }
 
   /**
@@ -77,18 +80,24 @@ public abstract class PlayerBoard {
    * @param player The player that did mark
    */
   public void setMark(Player player) {
+    marksAssigned.add(player);
   }
 
   /**
    * Flip the board, changing scores index
    */
   public void turnAround() {
+    deathValues.clear();
+    deathValues.add(2);
+    deathValues.add(1);
+    isFrenzy = true;
   }
 
   /**
-   * @return The current side of the board
+   * @return if the board is on final frenzy mode state
    */
-  public boolean getBoardSide() {
+  public boolean getIfIsFrenzy() {
+    return isFrenzy;
   }
 }
 
