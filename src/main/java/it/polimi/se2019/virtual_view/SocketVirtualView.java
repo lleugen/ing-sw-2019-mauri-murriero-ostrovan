09eba043796d766223e3b8f5ae65497c88a14986
Fabio Mauri <cripty2001@gmail.com>
2019-05-09 20:29:06 +0200

    Add Virtual View (local+socket)

---
+++ src/main/java/it/polimi/se2019/virtual_view/SocketVirtualView.java
@@ -0,0 +1,292 @@
+package it.polimi.se2019.virtual_view;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.logging.Logger;
+
+// TODO: Optimization, not ready yet for production
+
+/**
+ * Socket protocol is really simple
+ *
+ * Every message is sent over a TCP connection, opened at the beginning of the
+ * session, and kept open.
+ * Every message is sent encoded in base64, and ends with the character `~`
+ *
+ * A message is formed by three pieces of data, separated by the character `~`:
+ * - An action identifier, chosen between:
+ * -- MODEL_UPDATE  (server -> client)
+ * -- EXCEPTION     (server -> client)
+ * -- VIEW_SELECT   (server -> client)
+ * -- METHOD_INVOC  (client -> server)
+ * - The path of the generator
+ * - The real message
+ * An example of a message not encoded is:
+ * ```MODEL_UPDATE~server.map.cells~data to pass```
+ * An example of a message encoded (the same as above) is:
+ * ```TU9ERUxfVVBEQVRFfnNlcnZlci5tYXAuY2VsbHN+ZGF0YSB0byBwYXNz~```.
+ * Please note the `~` at the end of the message
+ */
+public class SocketVirtualView extends AbstractVirtualView {
+  /**
+   * Sequence of updates sent down to the client.
+   * Useful to handle client disconnection and reconnection
+   */
+  private ObservableList<byte[]> downstreamLog;
+
+  /**
+   * Inits a new local virtual view.
+   *
+   * A local virtual view can be used as a child view of another virtual view.
+   * It doesn't have any remote logic, and transfer any work to the parent view.
+   * Therefore, while initializing a LocalVirtualView, you must pass to it
+   * a function used to pass model updates to the parent view
+   *
+   * @param namespace Namespace of the view
+   * @param port Port to which bind the socket
+   *
+   * @throws SocketInitializationError when there is an error while
+   *                                   initializing the socket
+   */
+  public SocketVirtualView(String namespace, Integer port) {
+    super(namespace);
+
+    this.downstreamLog = new ObservableList<>();
+
+    try {
+      new ConnectionHandler(
+              new ServerSocket(port),
+              (Consumer<byte[]> h) -> this.downstreamLog.subscribe(h),
+              this::call
+      ).run();
+    }
+    catch (java.io.IOException e){
+      throw new SocketInitializationError(e);
+    }
+  }
+
+  /**
+   * Push a state to the view
+   *
+   * @param path Path for the view (the same as the model)
+   * @param data Data to push to the view
+   */
+  public void pushState(String path, String data){
+    this.broadcastMessage("MODEL_UPDATE", path, data);
+  }
+
+  /**
+   * Push a state to the view.
+   * This function can be used to push updates from child views.
+   *
+   * @param path Path of the view to deliver the request to
+   * @param view Name of the view to show
+   */
+  public void setView(String path, String view){
+    this.broadcastMessage("VIEW_SELECT", path, view);
+  }
+
+  /**
+   * Broadcast a message to all connected client
+   * @param action
+   * @param path  Path relative, namespace is added automatically
+   * @param data
+   */
+  private void broadcastMessage(String action, String path, String data){
+    String packet = action + "~" + this.addNamespace(path) + "~" + data;
+    this.downstreamLog.add(
+            java.util.Base64.getEncoder().encode(packet.getBytes())
+    );
+  }
+
+  private static class ConnectionHandler implements Runnable {
+    /**
+     * The socket used to accept connections
+     */
+    private ServerSocket serverSocket;
+
+    /**
+     * True if connection loop should be enabled, false otherwise
+     */
+    private Boolean looping;
+
+    private Consumer<Consumer<byte[]>> downstramHandler;
+
+    private BiConsumer<String, String> upstreamHandler;
+
+    /**
+     *
+     * @param ss
+     * @param dh
+     */
+    ConnectionHandler(
+            ServerSocket ss, Consumer<Consumer<byte[]>> dh, BiConsumer<String, String> uh){
+      this.serverSocket = ss;
+      this.looping = true;
+      this.downstramHandler = dh;
+      this.upstreamHandler = uh;
+    }
+
+    /**
+     * Stop looping the connection listening
+     */
+    public void stopLoop(){
+      looping = false;
+    }
+
+    public void run(){
+      while (this.looping){
+        try (Socket socket = serverSocket.accept()){
+          new UpstreamClientHandler(
+                  new DataInputStream(socket.getInputStream()),
+                  this.upstreamHandler
+          ).run();
+          this.downstramHandler.accept(
+                  DownstreamClientHandler.getHandler(
+                          new DataOutputStream(socket.getOutputStream()),
+                          socket
+                  )
+          );
+        }
+        catch (java.io.IOException e){
+          SocketVirtualView.logSocketException(e);
+        }
+      }
+    }
+  }
+
+  private static class UpstreamClientHandler implements Runnable{
+    private DataInputStream inputStream;
+    private BiConsumer<String, String> processor;
+
+    UpstreamClientHandler(DataInputStream in, BiConsumer<String, String> f){
+      this.inputStream = in;
+      this.processor = f;
+    }
+
+    public void run(){
+      char c;
+      StringBuilder gotString = new StringBuilder();
+      byte[] decoded;
+      String[] decodedParsed;
+      boolean loop = true;
+
+      while (loop) {
+        try {
+          c = this.inputStream.readChar();
+
+          if ('~' == c){
+            // Packet ended
+            decoded = java.util.Base64.getDecoder().decode(gotString.toString());
+            decodedParsed = Arrays.toString(decoded).split("~");
+            this.processDataHandler(decodedParsed);
+
+            gotString.setLength(0);
+          }
+          else {
+            gotString.append(c);
+          }
+        }
+        catch (java.io.IOException e){
+          loop = false;
+          logSocketException(e);
+        }
+      }
+
+      // Leaving thread to the garbage collector
+    }
+
+    private void processDataHandler(String[] data){
+      if ("METHOD_INVOC".equals(data[0])) {
+        this.processor.accept(data[1], data[2]);
+      }
+    }
+  }
+
+  private static class DownstreamClientHandler {
+    static Consumer<byte[]> getHandler(OutputStream out, Socket s){
+      return (byte[] data) -> {
+        try {
+          out.write(data);
+        }
+        catch (java.io.IOException e){
+          logSocketException(s, e);
+        }
+      };
+    }
+  }
+
+  /**
+   * Thrown when there is an error while initializing the socket for this
+   * virtual view
+   */
+  public static class SocketInitializationError extends RuntimeException {
+    SocketInitializationError(Exception e){
+      super(e);
+    }
+
+    @Override
+    public String toString() {
+      return "Error while initializing the socket";
+    }
+  }
+
+  private static void logSocketException(Exception e){
+    Logger.getGlobal().throwing(
+            SocketVirtualView.class.getCanonicalName(),
+            "logSocketException",
+            e
+    );
+  }
+
+  private static void logSocketException(Socket s, Exception e){
+    logSocketException(e);
+
+    try {
+      s.close();
+    }
+    catch (java.io.IOException ex){
+      logSocketException(ex);
+    }
+  }
+
+  public static class ObservableList<P extends Object> {
+    private List<P> list;
+    private List<Consumer<P>> handlerList;
+
+    ObservableList() {
+      this.list = Collections.synchronizedList(new ArrayList<>());
+      this.handlerList = Collections.synchronizedList(new ArrayList<>());
+    }
+
+    public void add(P data){
+      int listStart = this.list.size();
+
+      this.list.add(data);
+
+      this.handlerList.forEach(
+              (Consumer<P> handler) -> this.notify(listStart, handler)
+      );
+    }
+
+    void subscribe(Consumer<P> handler){
+      this.handlerList.add(handler);
+      this.notify(0, handler);
+    }
+
+    private void notify(int listId, Consumer<P> handler){
+      this.list.subList(listId, this.list.size()).forEach(
+              handler
+      );
+    }
+  }
+}
---
+++ src/main/java/it/polimi/se2019/virtual_view/VirtualViewInterface.java
@@ -0,0 +1,54 @@
+package it.polimi.se2019.virtual_view;
+
+import java.util.function.Consumer;
+
+public interface VirtualViewInterface {
+  /**
+   * Ask the view to show another view
+   *
+   * @param path Path of the view to deliver the request to
+   * @param view Name of the view to show
+   */
+  void setView(String path, String view);
+
+  /**
+   * Push a state to the view.
+   * This function can be used to push updates from child views.
+   *
+   * @param path    Path to which send the update (relative)
+   * @param state   The state to push to the view
+   */
+  void pushState(String path, String state);
+
+  /**
+   * Add a new callable method to the virtual view
+   *
+   * @param name     Name of the method to expose to the view
+   * @param function Method to call on the controller
+   */
+  void addMethod(String name, Consumer<String> function);
+
+  /**
+   * Add a new related view to the virtual view
+   *
+   * @param name Name of the view to add
+   * @param view Related virtual view to add to this view
+   */
+  void addRelatedView(String name, VirtualViewInterface view);
+
+  /**
+   * Call a method on the current view associated controller or in related
+   * virtual views.
+   * You can access related view by using namespaces.
+   * For example, if you have the main view initialized as v1, with method v1m1,
+   * and you have the related view v2 added to v1 with name v2, you can access
+   * method v2m1 (method on view 2), by passing as path to this function v1.v2.m1
+   *
+   * __WARN__: Namespace is required
+   * __WARN__: If a method doesn't exists, no exception is returned
+   *
+   * @param path Path of the method to call
+   * @param data Data to pass to the called method as parameter
+   */
+  void call(String path, String data);
+}
